267eae3f3a7c87fb3aaab258e6c36139f3172757
unknown <faizal@webcastle.in>
2025-03-17 14:27:44 +0530

    update db creds

--- database/db.php
+++ database/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$database = "user_system";
+$host = "sql12.freesqldatabase.com";
+$user = "sql12767771";
+$pass = "Fcw9NmDp1A";
+$database = "sql12767771";
 
 $connection = mysqli_connect($host, $user, $pass, $database);
 
